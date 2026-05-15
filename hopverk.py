15ada3905f23c89c07c2c8308a28963152b8b63b
dannimanni <danielrafn122@hotmail.com>
2026-05-15 13:16:58 +0000

    Update hopverk.py

--- hopverk.py
+++ hopverk.py
@@ -9,26 +9,23 @@ led = Pin(11, Pin.OUT)
 takki = Pin(12, Pin.IN, Pin.PULL_UP)
 
 
+randomtala = random.randint(1,6)
+i2c = SoftI2C(scl=Pin(13), sda=Pin(14), freq=400000)
+# print(i2c.scan()) # sýnir addressurnar á skjáunum sem eru tengdir við 13 og 14
+#lcd = I2cLcd(i2c, 0x3f, 2, 16)
+# EÐA ef þú færð villu á línuna hér fyrir ofan
+lcd = I2cLcd(i2c, 39, 2, 16)
 
-                                                    
+# Færi bendilinn í staf nr. 0 og línu nr. 0
+                                         
 while True:
     if takki.value() == 0:
-                # Skjárinn nota I2C tengingu til að tala við ESP
-        randomtala = random.randint(1,6)
-        i2c = SoftI2C(scl=Pin(13), sda=Pin(14), freq=400000)
-        # print(i2c.scan()) # sýnir addressurnar á skjáunum sem eru tengdir við 13 og 14
-        #lcd = I2cLcd(i2c, 0x3f, 2, 16)
-        # EÐA ef þú færð villu á línuna hér fyrir ofan
-        lcd = I2cLcd(i2c, 39, 2, 16)
-
-        # Færi bendilinn í staf nr. 0 og línu nr. 0
-        lcd.move_to(0, 0)
-        lcd.putstr(str(randomtala))
+        lcd.move_to(0, 0)  
+        lcd.putstr(str(randomtala))   
         print(randomtala)
-
-
+        
         if randomtala == 6:
             led.value(1)
-            sleep_ms(500)
+            sleep_ms(100)
     sleep_ms(100)
     led.value(0)
